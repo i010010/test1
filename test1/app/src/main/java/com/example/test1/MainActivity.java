9323209c6eca82c7c3b7905f865c0edc93d8038f
i010010 <colorofflame@gmail.com>
2019-03-24 23:12:27 +0900

    ImageView INVISIBLE

--- test1/app/src/main/java/com/example/test1/MainActivity.java
+++ test1/app/src/main/java/com/example/test1/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +12,15 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button button = findViewById(R.id.button);
+
+        final ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.neko1);
+
 
         button.setOnClickListener(new View.OnClickListener() {
 
@@ -23,12 +29,17 @@ public class MainActivity extends AppCompatActivity {
                 // flagがtrueの時
                 if (flag) {
 
+                    imageView.setVisibility(View.INVISIBLE);
+                    flag = false;
 
                 } else {
 
+                    imageView.setVisibility(View.VISIBLE);
+                    flag = true;
 
                 }
             }
+
         });
 
 
